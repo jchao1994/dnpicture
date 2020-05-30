a8b42268a06302a8bd679408bb95fb5fd1619868
jc <305052315@qq.com>
2020-05-30 09:38:00 +0800

    '备份'

---
+++ src/components/tabs/tabs.vue
@@ -0,0 +1,39 @@
+<template>
+  <view class="tabs">
+    tabs
+    <uni-segmented-control :current="current" :values="items" @clickItem="onClickItem"></uni-segmented-control>
+  </view>
+</template>
+
+<script>
+import {uniSegmentedControl} from "@dcloudio/uni-ui"
+
+export default {
+  data() {
+    return {
+      current: 0
+    }
+  },
+  props: {
+    items: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  methods: {
+    onClickItem(e) {
+      if (this.current !== e.currentIndex) {
+        this.current = e.currentIndex;
+      }
+    }
+  },
+  components: {
+    uniSegmentedControl
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="home">
+    <tabs :items="tabsItems"></tabs>
+    <home-recommend></home-recommend>
+    <home-category></home-category>
+    <home-new></home-new>
+    <home-album></home-album>
+  </view>
+</template>
+
+<script>
+  import homeRecommend from './childComps/home-recommend/homeRecommend';
+  import homeCategory from './childComps/home-category/homeCategory';
+  import homeNew from './childComps/home-new/homeNew';
+  import homeAlbum from './childComps/home-album/homeAlbum';
+
+  import tabs from '@/components/tabs/tabs';
+
+export default {
+  data() {
+    return {
+      tabsItems: ['选项卡1','选项卡2','选项卡3']
+    }
+  },
+  components: {
+    homeRecommend,
+    homeCategory,
+    homeNew,
+    homeAlbum,
+    tabs
+  }
+}
+</script>
+
+<style>
+
+</style>
